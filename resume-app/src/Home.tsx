db2959856da915357711011c65e812c521c67bbc
Joshua Cookhorne <jncookhorne@gmail.com>
2024-03-17 16:29:47 -0400

    uhm just inserted an image and got bootstrap sort of working?

--- resume-app/src/Home.tsx
+++ resume-app/src/Home.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import image1 from "./images/bnwUs.jpg";
 
 
 const Home = () =>{
+
+useEffect(() => {
+// console.log(image1)
+}, [])
 
 
 
 return(
     <>
+  <div className="">
+    
+  </div>
+    
     <div className=" homeClass">
         <h5>THE  WORLD</h5>
-    </div>
+        <img className="bnwUs pl-5 ml-5" src={image1} alt="bnwUs"/>
+    </div>    
 
 
     </>
